f968d0dab26a3f4cfeb71e325fd8c5573ef49380
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-05-06 17:47:46 +0900

    pageDto추가

---
+++ src/main/java/demo/reservation/common/dto/PageDto.java
@@ -0,0 +1,37 @@
+package demo.reservation.common.dto;
+
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+public class PageDto {
+  int page;
+
+  int size;
+
+  boolean isAsc;
+
+  String sortBy;
+
+  public Pageable toPageable() {
+    if(Objects.nonNull(sortBy)){
+      return isAsc ? PageRequest.of(page, size,
+          Sort.by(sortBy).descending()) : PageRequest.of(page, size,
+          Sort.by(sortBy).ascending());
+    }
+    return isAsc ? PageRequest.of(page, size,
+        Sort.by("createdAt").descending()) : PageRequest.of(page, size,
+        Sort.by("createdAt").ascending());
+  }
+
+}
+
