0203a0ef356bdc2929de4ac9d8459e2813022288
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-05-06 17:50:52 +0900

    reservationItemRepository가 받는 상속에 reservationItemQuery 추가

--- src/main/java/demo/reservation/reservationItem/dao/ReservationItemRepository.java
+++ src/main/java/demo/reservation/reservationItem/dao/ReservationItemRepository.java
@@ -3,6 +3,6 @@ package demo.reservation.reservationItem.dao;
 import demo.reservation.reservationItem.entity.ReservationItem;
 import org.springframework.data.repository.Repository;
 
-public interface ReservationItemRepository extends Repository<ReservationItem,Long> {
+public interface ReservationItemRepository extends Repository<ReservationItem,Long>,ReservationItemRepositoryQuery {
   void save(ReservationItem reservationItem);
 }
