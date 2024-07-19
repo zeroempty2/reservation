e9273d614e37f3ae0bb989abdcdc32044b0c61b9
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-07-19 19:56:06 +0900

    몽고db 관련 파일 제거

--- src/main/java/demo/reservation/mongoDB/MongoDBTestRepository.java
+++
@@ -1,8 +0,0 @@
-//package demo.reservation.mongoDB;
-//
-//
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//
-//public interface MongoDBTestRepository extends MongoRepository<MongoTest, String> {
-//  MongoTest findByName(String name);
-//}
