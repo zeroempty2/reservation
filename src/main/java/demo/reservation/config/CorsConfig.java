35a3651ba116be0573b2e25a4aa6c78db04518d0
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-07-19 19:57:03 +0900

    cors설정 변

---
+++ src/main/java/demo/reservation/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package demo.reservation.config;
+
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+@Configuration
+public class CorsConfig {
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("http://localhost:3000");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    config.setExposedHeaders(Arrays.asList("Authorization"));
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+}
