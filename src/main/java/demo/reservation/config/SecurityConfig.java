b848c3f79c73f3d27c3ea951540645a863b5bf1f
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-07-19 19:56:41 +0900

    security설정 변

--- src/main/java/demo/reservation/config/SecurityConfig.java
+++ src/main/java/demo/reservation/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,7 +25,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @RequiredArgsConstructor
 @EnableWebSecurity
 @EnableMethodSecurity
-public class SecurityConfig {
+public class SecurityConfig{
   private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
   private final JwtUtil jwtUtil;
   private final UserDetailsServiceImpl userDetailsService;
@@ -52,16 +51,17 @@ public class SecurityConfig {
         .addFilterBefore(new JwtAuthFilter(jwtUtil, userDetailsService), UsernamePasswordAuthenticationFilter.class)
         .sessionManagement(session -> session
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        )
-        .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-            .requestMatchers("/**").permitAll()
-            .anyRequest().authenticated()
-        )  .exceptionHandling(exceptionHandling -> exceptionHandling
-            .authenticationEntryPoint(customAuthenticationEntryPoint) // 인증 실패 시 처리할 핸들러 지정
-            .accessDeniedHandler(customAccessDeniedHandler) // 권한 부족 시 처리할 핸들러 지정
         );
+//        .authorizeHttpRequests(authorize -> authorize
+//            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//            .requestMatchers("/users/**").permitAll()
+//            .anyRequest().authenticated()
+//        )  .exceptionHandling(exceptionHandling -> exceptionHandling
+//            .authenticationEntryPoint(customAuthenticationEntryPoint) // 인증 실패 시 처리할 핸들러 지정
+//            .accessDeniedHandler(customAccessDeniedHandler) // 권한 부족 시 처리할 핸들러 지정
+//        );
 
     return http.build();
   }
+
 }
